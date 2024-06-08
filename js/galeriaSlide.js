9d54fb244df73dc16dc2bd896dead26e7f5cc9fb
Matias Lascurain <matiaslascurain04@gmail.com>
2024-06-08 01:14:03 -0300

    footer added, styling a bit, and some other minor tweaks

--- js/galeriaSlide.js
+++ js/galeriaSlide.js
@@ -13,10 +13,16 @@ function currentSlide(n) {
 
 function showSlides(n) {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
+  let slides = document.getElementsByClassName("slides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
