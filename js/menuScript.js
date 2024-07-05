8a3cd2425693334e1e18f0ca06ae2b7ece98c5ae
Matias Lascurain <matiaslascurain04@gmail.com>
2024-07-05 19:43:24 -0300

    arreglando galeria y otros detalles

--- js/menuScript.js
+++ js/menuScript.js
@@ -5,3 +5,10 @@ $icon.addEventListener("click", () =>{
     $nav.classList.toggle("nav__ul-activate");
     $nav.classList.toggle("nav__ul");
 });
+
+window.addEventListener("resize", function(){
+    if(this.window.matchMedia("(min-width: 1200px)").matches){
+        $nav.classList.remove("nav__ul-activate");
+        $nav.classList.add("nav__ul");
+    }
+})
